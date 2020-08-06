abbad46fc6f2c806fee50456ee5888589957fb28
zhouwei1997 <hengyu19971023@163.com>
2020-08-06 18:12:43 +0800

    Iterator迭代器

--- src/com/hengyu/collection/IteratorDemo.java
+++ src/com/hengyu/collection/IteratorDemo.java
@@ -4,7 +4,9 @@ package com.hengyu.collection;
  * @Author:hengyu
  * @Date:2020/8/6 18:07
  * @Version:1.0
- * @Discription: Iterator迭代器
+ * @Discription: Iterator迭代
+ *
+ * java.util.Iterator接口：迭代器（对集合进行遍历）
  */
 public class IteratorDemo {
 }
