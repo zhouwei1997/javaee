eb4697bb77d5193ea0c2fd448c60f1204ece5102
zhouwei1997 <zhouweizhoujie1997>
2020-08-06 21:19:49 +0800

    泛型

---
+++ src/com/hengyu/generic/GenericDemo.java
@@ -0,0 +1,14 @@
+package com.hengyu.generic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.generic
+ * ClassName: GenericDemo
+ * Author: hengyu
+ * Date: 2020/8/6 21:14
+ * project name: javase
+ * Version:
+ * Description:
+ */
+public class GenericDemo {
+}
