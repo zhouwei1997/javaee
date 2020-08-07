17100146ba694d22a9c93076eb7df9984293324b
zhouwei1997 <zhouweizhoujie1997>
2020-08-07 22:30:14 +0800

    定义含有泛型的方法

---
+++ src/com/hengyu/generic/GenericMethon.java
@@ -0,0 +1,15 @@
+package com.hengyu.generic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.generic
+ * ClassName: GenericMethon
+ * Author: hengyu
+ * Date: 2020/8/7 22:28
+ * project name: javase
+ * Version:
+ * Description:
+ * 定义含有泛型的方法，泛型定义在方法的修饰符和返回值类型上
+ */
+public class GenericMethon {
+}
