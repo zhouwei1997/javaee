8e24f5b7f39585a0de1eecdb6fd7b71ff7f9ba1e
zhouwei1997 <zhouweizhoujie1997>
2020-08-09 17:03:37 +0800

    定义含有泛型的接口

---
+++ src/com/hengyu/generic/GenericInterface.java
@@ -0,0 +1,16 @@
+package com.hengyu.generic;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.generic
+ * ClassName: GenericInterface
+ * Author: hengyu
+ * Date: 2020/8/9 16:08
+ * project name: javase
+ * Version:
+ * Description: 含有泛型的接口
+ */
+public interface GenericInterface<I> {
+
+    public abstract void method(I i);
+}
