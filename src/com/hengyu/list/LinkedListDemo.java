59e69f6b7aacf7b6a0d6f9cb7b16c01c1020313a
zhouwei1997 <zhouweizhoujie1997>
2020-08-10 21:41:36 +0800

    LinkedList 接口

---
+++ src/com/hengyu/list/LinkedListDemo.java
@@ -0,0 +1,32 @@
+package com.hengyu.list;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.list
+ * ClassName: LinkedListDemo
+ * Author: hengyu
+ * Date: 2020/8/10 21:33
+ * project name: javase
+ * Version:
+ * Description:
+ *
+ * java.util.LinkedList集合 implement List接口
+ *
+ * LinkedList集合的特点：
+ *  1、底层是一个链表结构：查询慢，增删快
+ *  2、里面包含了大量的首尾元素的方法
+ *
+ *  public void addFirst(E e)：将指定元素插入到此列表的开头
+ *  public void addLast(E e)：将指定元素插入到此列表的结尾
+ *  public E getFirst()：返回此列表的第一个元素
+ *  public E getLast()：返回此列表的最后一个元素
+ *  public E removeFirst()：移除此列表的第一个元素
+ *  public E removeLast()：移除此列表的最后一个元素
+ *  public E pop()：从此列表所表示的堆栈中弹出一个元素
+ *  public void push(E e)：将元素推入此列表所表示的堆栈
+ *  public Boolean isEmpty()：如果列表中不包含元素，则返回true
+ */
+public class LinkedListDemo {
+
+
+}
