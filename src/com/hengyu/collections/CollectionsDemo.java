2a559348bfa9f1d1a67a3e8cfb9c52da17986323
zhouwei1997 <zhouweizhoujie1997>
2020-08-11 22:56:13 +0800

    Collections集合工具类

--- src/com/hengyu/collections/CollectionsDemo.java
+++ src/com/hengyu/collections/CollectionsDemo.java
@@ -1,5 +1,7 @@
 package com.hengyu.collections;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -15,18 +17,35 @@ import java.util.Collections;
  */
 public class CollectionsDemo {
 
-    public static void main(String[] args) {
+    private ArrayList<String> list = new ArrayList<>();
 
-        ArrayList<String> list = new ArrayList<>();
-        //往集合中添加多个元素
-//        list.add("a");
-//        list.add("b");
-//        list.add("c");
-//        list.add("d");
-//        list.add("e");
-
-        //使用addAll完成上面的操作
+    /**
+     * 使用public static <T> boolean addAll(Collection<T> c,T... elements)向集合中添加多个元素
+     */
+    @Test
+    public void addAllTest() {
         Collections.addAll(list, "a", "b", "c", "d", "e");
+        System.out.println(list);
+    }
+
+    /**
+     * 使用public static void shuffle(List<?> list)打乱集合中的顺序
+     */
+    @Test
+    public void shuffleTest() {
+        addAllTest();
+        Collections.shuffle(list);
+        System.out.println(list);
+    }
+
+    /**
+     * 使用public static <T> void sort(List<T> list);对集合中的元素进行排序
+     */
+    @Test
+    public void sortTest(){
+        Collections.addAll(list, "1", "3", "2", "9", "8");
+        System.out.println(list);
+        Collections.sort(list);
         System.out.println(list);
     }
 }
--- src/com/hengyu/collections/Collections集合工具类.md
+++ src/com/hengyu/collections/Collections集合工具类.md
@@ -8,4 +8,15 @@ public static <T> boolean addAll(Collection<T> c,T... elements);
 打乱顺序，打乱集合顺序
 public static void shuffle(List<?> list);
 
+将集合中元素按照默认规则排序
+public static <T> void sort(List<T> list);
+
+将集合中元素按照指定规则排序
+public static <T> void sort(List<T> list,Comparator<? super T> );
+
+
+## Comparator和Comparable的区别
+Comparable：this和别人的参数比较，自己需要实现Comparable接口，重写比较的规则compareTo方法
+
+Compartor：相当于找一个第三方的裁判，比较两个
 
