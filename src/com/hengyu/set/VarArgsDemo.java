36968ed5b3ebb0e1d1eb9878a69ac75e5a5d921e
zhouwei1997 <zhouweizhoujie1997>
2020-08-11 22:15:09 +0800

    可变参数

---
+++ src/com/hengyu/set/VarArgsDemo.java
@@ -0,0 +1,14 @@
+package com.hengyu.set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.set
+ * ClassName: VarArgsDemo
+ * Author: hengyu
+ * Date: 2020/8/11 22:10
+ * project name: javase
+ * Version:
+ * Description: 可变参数
+ */
+public class VarArgsDemo {
+}
