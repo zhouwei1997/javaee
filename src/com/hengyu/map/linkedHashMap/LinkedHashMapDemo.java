fc28b929529d9e113253de491902401d9e82f787
zhouwei1997 <zhouweizhoujie1997>
2020-08-12 22:37:57 +0800

    LinkedHashMap集合

---
+++ src/com/hengyu/map/linkedHashMap/LinkedHashMapDemo.java
@@ -0,0 +1,37 @@
+package com.hengyu.map.linkedHashMap;
+
+import org.junit.Test;
+
+import javax.print.DocFlavor;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.map.linkedHashMap
+ * ClassName: LinkedHashMapDemo
+ * Author: hengyu
+ * Date: 2020/8/12 22:33
+ * project name: javase
+ * Version:
+ * Description:
+ */
+public class LinkedHashMapDemo {
+
+    @Test
+    public void test() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("a", "a");
+        map.put("c", "c");
+        map.put("b", "b");
+        map.put("d", "d");
+        System.out.println(map);
+        System.out.println("====================================");
+        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("a", "a");
+        linkedHashMap.put("c", "c");
+        linkedHashMap.put("b", "b");
+        linkedHashMap.put("d", "d");
+        System.out.println(linkedHashMap);
+    }
+}
