070fda159194840a5a6ae7d97f6e7d226b2b931a
zhouwei1997 <zhouweizhoujie1997>
2020-08-13 21:44:36 +0800

    异常

---
+++ src/com/hengyu/thread/RegisterException.java
@@ -0,0 +1,31 @@
+package com.hengyu.thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.thread
+ * ClassName: RegisterException
+ * Author: hengyu
+ * Date: 2020/8/13 20:23
+ * project name: javase
+ * Version:
+ * Description:自定义异常
+ * <p>
+ * 自定义异常需要继承Exception或者RuntimeException
+ */
+public class RegisterException extends Exception {
+
+    /**
+     * 添加一个空参数的构造方法
+     */
+    public RegisterException() {
+    }
+
+    /**
+     * 添加一个带异常信息的构造方法
+     * 所有异常类都会有一个带异常信息的构造方法，方法内部会调用父类异常信息的构造方法，让父类来处理这个异常信息
+     */
+    public RegisterException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/com/hengyu/thread/RegisterExceptionDemo.java
@@ -0,0 +1,42 @@
+package com.hengyu.thread;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * PackgeName: com.hengyu.thread
+ * ClassName: RegisterException
+ * Author: hengyu
+ * Date: 2020/8/13 20:23
+ * project name: javase
+ * Version:
+ * Description:模拟注册操作，如果用户名已存在，则抛出异常信息
+ */
+public class RegisterExceptionDemo {
+
+    /**
+     * 使用数组保存已经注册过的用户名
+     */
+    static String[] usernames = {"张三", "李四", "王文英", "刘小强"};
+
+    public static void main(String[] args) throws RegisterException {
+        //使用Scanner获取用户输入的注册的用户名
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入需要注册的用户名：");
+        String username = scanner.next();
+        checkUsername(username);
+    }
+
+    /**
+     * 对用户输入的注册的用户名进行判断
+     */
+    private static void checkUsername(String username) throws RegisterException {
+        //遍历存储已经注册过的用户名数组，获取每一个用户名
+        for (String name : usernames) {
+            if (name.equals(username)) {
+                //用户名操作，抛出RegisterException异常
+                throw new RegisterException("该用户名已被注册");
+            }
+        }
+    }
+}
